e54a8e8ad4d7cc9953fbacc70a8bd09771babdb5
DonKoutsou <anthonykoutsouradis@gmail.com>
2023-11-12 20:48:47 +0100

    stuff

--- Scripts/Game/Dialogue/Actions/SP_DialogueStageHealAction.c
+++ Scripts/Game/Dialogue/Actions/SP_DialogueStageHealAction.c
@@ -1,10 +1,23 @@
 [BaseContainerProps(configRoot:true), DialogueStageTitleAttribute()]
 class DialogueStageHealAction : DialogueStage
 {
+	SCR_FadeInOutEffect FadeInOutEffect;
 	override void Perform(IEntity Character, IEntity Player)
 	{
+		SCR_PlayerController playerController = SCR_PlayerController.Cast(GetGame().GetPlayerController());
+		if (!playerController)
+			return;
+		
+		SCR_HUDManagerComponent hudManager = SCR_HUDManagerComponent.Cast(playerController.GetHUDManagerComponent());
+		if (!hudManager)
+			return;
+		FadeInOutEffect = SCR_FadeInOutEffect.Cast(hudManager.FindInfoDisplay(SCR_FadeInOutEffect));
+		FadeInOutEffect.FadeOutEffect(false, 1000);
+		GetGame().GetCallqueue().CallLater(Fadeout, 1000, false);
+		
 		SCR_CharacterDamageManagerComponent CharDamageMnagr = SCR_CharacterDamageManagerComponent.Cast(Player.FindComponent(SCR_CharacterDamageManagerComponent));
 		CharDamageMnagr.FullHeal();
+		
 		super.Perform(Character, Player);
 	};
 	override bool CanBePerformed(IEntity Character, IEntity Player)
@@ -16,5 +29,9 @@ class DialogueStageHealAction : DialogueStage
 			return false;
 		}
 		return true;
+	}
+	void Fadeout()
+	{
+		FadeInOutEffect.FadeOutEffect(false, 10);
 	}
 };
